b581eca89f82563c7a83bb87ff24fde66aa5116c
Brian Nelson <yonbeastie@gmail.com>
2013-06-24 18:01:27 -0500

    make connector_v1 compliant with new API
    Signed-off-by: Brian Nelson <yonbeastie@gmail.com>

--- connectors/connector_v1.php
+++ connectors/connector_v1.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Version: 1.6
+ * Version: 1.8
  *
  * Changelog:
  * 1.0 Initial release
@@ -13,12 +13,13 @@
  * 1.6 Added link redirects to Single Call
  * 1.6.1 Removed the link redirects added in 1.6
  * 1.7 Added config api call
+ * 1.8 Integrate with new API
  */
 
 class Todaycms {
 	public $client = '';
 	private $server = 'http://launch.todaymade.com';
-	private $api_url = '';
+	private $api_url = 'http://todaycms-api.herokuapp.com';
 	private $debug = false;
 	private $config = false;
 	private $formbuilder_init = false;
@@ -26,18 +27,23 @@ class Todaycms {
 	public function __construct($client = '') {
 		$this -> client = $client;
 
+		// Error
+		if (isset($_GET['error'])) {
+			ini_set('display_errors', 1);
+			ini_set('log_errors', 1);
+			ini_set('error_log', dirname(__FILE__) . '/error_log.txt');
+			error_reporting(E_ALL);
+		}
 		// Debug
 		if (isset($_GET['debug'])) {
 			$this -> debug = true;
 		}
-
 		//	Staging Server
 		if (isset($_GET['stage'])) {
 			$this -> server = 'http://stage.todaymade.com';
 		}
 
-		$this -> api_url = $this -> server . '/api/' . $client . '/';
-		$this -> social_url = $this -> server . '/social/';
+		$this->config();
 	}
 
 	public function config($key = false) {
@@ -54,45 +60,73 @@ class Todaycms {
 	}
 
 	public function parent($key) {
-		$url = 'parent/key:' . $key;
-		return $this -> get($this -> api_url . $url);
+		throw new Exception('CMS PHP SDK no longer supports ->parent() ');
 	}
 
-	public function multiple($parent, $params = array()) {
-		$url = 'multiple/parent:' . $parent;
-		foreach ($params as $k => $v) {
+	public function multiple($parent, $p = array()) {
+		// Params
+		$params = '';
+		foreach ($p as $k => $v) {
 			if ($k == 'filter') {
-				$url .= '/filter:';
 				$filters = array();
 				foreach ($v as $field => $value) {
-					$filters[] = $field . '--' . $value;
+					$filters[$field] = $value;
 				}
-				$url .= implode('||', $filters);
+				$params .= '&filter='. urlencode(json_encode(($filters)));
 			} else {
-				$url .= '/' . $k . ':' . $v;
+				$params .= '&' . $k . '=' . $v;
 			}
 		}
-		return $this -> get($this -> api_url . $url);
+		// Collection
+		$collection = $parent;
+
+		$data = $this -> get($this->api_url.'/collections/'.$collection.'?_token='.$this->client . $params);
+
+		if ($data) {
+			for ($i=0; $i<count($data); $i++) {
+				$data[$i] = $this->append_url($data[$i]);
+				$data[$i] = $this->append_title($data[$i]);
+			}
+			return $data;
+		} else {
+			return false;
+		}
 	}
 
-	public function single($params) {
-		$url = 'single';
-		foreach ($params as $k => $v) {
-			$url .= '/' . $k . ':' . $v;
+	public function single($p) {
+		// Params
+		$params = '';
+		foreach ($p as $k => $v) {
+			if ($k == 'slug') {
+				$params .= '&slug=' . $v;
+			} elseif {
+				$this->parent = $v;
+			} else {
+				$params .= '&' . $k . '=' . $v;
+			}
 		}
 
-		return $this -> get($this -> api_url . $url);
+		if (!$this->parent) {
+			throw new Exception('CMS PHP SDK call to ->single() requires parent collection to be defined ');
+		} else {
+			$collection = $this->parent;
+		}
+
+		$data = $this -> get($this->api_url.'/collections/'.$collection.'?_token='.$this->client . $params);
+
+		if ($data) {
+			$data[0] = $this->append_url($data[0]);
+			$data[0] = $this->append_title($data[0]);
+			return $data[0];
+		} else {
+			return false;
+		}
+
+		return $this -> load($url);
 	}
 
 	public function social($service, $call = '', $params = array()) {
-		$url = $this -> social_url . $service . '/';
-		if (!empty($call)) {
-			$url .= trim($call, '/') . '/';
-		}
-		foreach ($params as $key => $val) {
-			$url .= $key . ':' . $val . '/';
-		}
-		return $this -> get($url);
+		throw new Exception('CMS PHP SDK no longer supports ->social() ');
 	}
 
 	public function save($type, $data) {
@@ -101,13 +135,14 @@ class Todaycms {
 	}
 
 	public function outline() {
-		return $this -> get($this -> api_url . 'outline');
+		throw new Exception('CMS PHP SDK no longer supports ->outline() ');
 	}
 
 	public function formbuilder($form = false, $page = false) {
 		if (!$this -> formbuilder_init) {
 			$this -> formbuilder_init = true;
-			echo '<script type="text/javascript" src="' . $this -> server . '/frontend/formbuilder.js"></script>';
+			//echo '<script type="text/javascript" src="' . $this -> server . '/frontend/formbuilder.js"></script>';
+			echo '<script type="text/javascript" src="http://launch.todaymade.com/frontend/formbuilder.js"></script>';
 		}
 
 		if ($form) {
@@ -141,6 +176,48 @@ class Todaycms {
 		$info = curl_getinfo($ch);
 		curl_close($ch);
 		return $output;
+	}
+
+	/** Helper methods to fill missing data (no longer automatically added by API)
+	************************************************************************************/
+
+	private function append_url($data) {
+		if ($data['status'] == 3) {
+			// Link Page
+			$data['url'] = $data['fields']['link'];
+            $data['url_target'] = $data['fields']['target'];
+		} else {
+			// Standard page
+			$url_config = (isset($this->config[$data['parent']]['url'])?$this->config[$data['parent']]['url']:true);
+            $data['url'] = $this->build_url($url_config, $data);
+            $data['url_target'] = '_self';
+		}
+
+		return $data;
+	}
+
+	private function build_url($url, $data) {
+		if ($url === true) {
+            return (!empty($data['parent']) ? '/' . $data['parent'] : '') . '/' . $data['slug'];
+        } else {
+            $url = str_replace('{{slug}}', $data['slug'], $url);
+            $url = str_replace('{{id}}', $data['id'], $url);
+            return str_replace('{{parent}}', $data['parent'], $url);
+        }
+	}
+
+	// Calculate the Title field based on first field in the object or slug
+	private function append_title($data) {
+		if (!isset($data['title'])) {
+			$title = '';
+			if (is_string($data['fields'][0])) {
+				$title = $data['fields'][0];
+			} elseif (isset($data['slug'])) {
+				$title = ucfirst(str_replace('-', ' ', $data['slug']));
+			}
+			$data['title'] = $title;
+		}
+		return $data;
 	}
 
 }
